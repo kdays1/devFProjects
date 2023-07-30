77b5a4efcee4cb4b215d78a1e92ab0f35f30b435
Kdays <caroodzg@gmail.com>
2023-07-30 15:19:21 -0600

    add nav-bar and logic to insert cards in the html

---
+++ 26CodeB-Kata3-FinalProyect/main.js
@@ -0,0 +1,147 @@
+const data = [
+    {
+        name: 'nombre X',
+        imageUrl: './_img/dratini.png',
+        type: ['Dragon'],
+        number: '147'
+    },
+    {
+        name: 'nombre X',
+        imageUrl: './_img/dratini.png',
+        type: ['Dragon'],
+        number: '147'
+    },
+    {
+        name: 'nombre X',
+        imageUrl: './_img/dratini.png',
+        type: ['Dragon'],
+        number: '147'
+    },
+    {
+        name: 'nombre X',
+        imageUrl: './_img/dratini.png',
+        type: ['Dragon'],
+        number: '147'
+    },
+    {
+        name: 'nombre X',
+        imageUrl: './_img/dratini.png',
+        type: ['Dragon'],
+        number: '147'
+    },
+    {
+        name: 'nombre X',
+        imageUrl: './_img/dratini.png',
+        type: ['Dragon'],
+        number: '147'
+    },
+    {
+        name: 'nombre X',
+        imageUrl: './_img/dratini.png',
+        type: ['Dragon'],
+        number: '147'
+    },
+    {
+        name: 'nombre X',
+        imageUrl: './_img/dratini.png',
+        type: ['Dragon'],
+        number: '147'
+    },
+    {
+        name: 'nombre X',
+        imageUrl: './_img/dratini.png',
+        type: ['Dragon'],
+        number: '147'
+    },
+    {
+        name: 'nombre X',
+        imageUrl: './_img/dratini.png',
+        type: ['Dragon'],
+        number: '147'
+    },
+    {
+        name: 'nombre X',
+        imageUrl: './_img/dratini.png',
+        type: ['Dragon'],
+        number: '147'
+    },
+    {
+        name: 'nombre X',
+        imageUrl: './_img/dratini.png',
+        type: ['Dragon'],
+        number: '147'
+    },
+    {
+        name: 'nombre X',
+        imageUrl: './_img/dratini.png',
+        type: ['Dragon'],
+        number: '147'
+    },
+    {
+        name: 'nombre X',
+        imageUrl: './_img/dratini.png',
+        type: ['Dragon'],
+        number: '147'
+    },
+    {
+        name: 'nombre X',
+        imageUrl: './_img/dratini.png',
+        type: ['Dragon'],
+        number: '147'
+    }
+]
+
+//Private Functions Region
+function createImageItem(url, name) {
+    let img = document.createElement('img');
+    img.src = url;
+    img.alt = name;
+    img.classList.add('card-img-top');
+
+    return img
+}
+
+function createCardTitle(name) {
+    let cardTitle = document.createElement('h5');
+    cardTitle.classList.add('card-title');
+    cardTitle.textContent = 'Name: ' + name;
+
+    return cardTitle
+}
+
+function createCardText(type, number) {
+    let cardText = document.createElement('p');
+    cardText.classList.add('card-text');
+    cardText.textContent = 'Number: ' + number;
+
+    return cardText
+}
+
+function createCardBody(_data) {
+    let cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    cardBody.appendChild(createCardTitle(_data.name));
+    cardBody.appendChild(createCardText(_data.type, _data.number))
+
+    return cardBody
+}
+
+function createCards() {
+    let container = document.querySelector('#cardsContainer');
+    for (let i=0; i<data.length;i++) {
+        let pokemon = document.createElement('card');
+        pokemon.classList.add('card');
+        pokemon.id = i;
+        pokemon.appendChild(createImageItem(data[i].imageUrl, data[i].name));
+        pokemon.appendChild(createCardBody(data[i]));
+        container.appendChild(pokemon);
+    }
+}
+//End Private Functions Region
+
+
+//Events Region
+document.addEventListener('DOMContentLoaded', function(evt) {
+    createCards();
+})
+//End events region
