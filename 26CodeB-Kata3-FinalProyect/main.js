b93e6f11661a564260e5a8f45a8ec341592ae051
Kdays <caroodzg@gmail.com>
2023-07-31 23:10:17 -0600

    modify functions to accept new objects structure and delete fake data

--- 26CodeB-Kata3-FinalProyect/main.js
+++ 26CodeB-Kata3-FinalProyect/main.js
@@ -1,105 +1,20 @@
 const PokemonTotalNumber = 151;
-
-const data = [
-    {
-        name: 'nombre X',
-        imageUrl: './_img/dratini.png',
-        type: ['Dragon'],
-        number: '147'
-    },
-    {
-        name: 'nombre X',
-        imageUrl: './_img/dratini.png',
-        type: ['Dragon'],
-        number: '147'
-    },
-    {
-        name: 'nombre X',
-        imageUrl: './_img/dratini.png',
-        type: ['Dragon'],
-        number: '147'
-    },
-    {
-        name: 'nombre X',
-        imageUrl: './_img/dratini.png',
-        type: ['Dragon'],
-        number: '147'
-    },
-    {
-        name: 'nombre X',
-        imageUrl: './_img/dratini.png',
-        type: ['Dragon'],
-        number: '147'
-    },
-    {
-        name: 'nombre X',
-        imageUrl: './_img/dratini.png',
-        type: ['Dragon'],
-        number: '147'
-    },
-    {
-        name: 'nombre X',
-        imageUrl: './_img/dratini.png',
-        type: ['Dragon'],
-        number: '147'
-    },
-    {
-        name: 'nombre X',
-        imageUrl: './_img/dratini.png',
-        type: ['Dragon'],
-        number: '147'
-    },
-    {
-        name: 'nombre X',
-        imageUrl: './_img/dratini.png',
-        type: ['Dragon'],
-        number: '147'
-    },
-    {
-        name: 'nombre X',
-        imageUrl: './_img/dratini.png',
-        type: ['Dragon'],
-        number: '147'
-    },
-    {
-        name: 'nombre X',
-        imageUrl: './_img/dratini.png',
-        type: ['Dragon'],
-        number: '147'
-    },
-    {
-        name: 'nombre X',
-        imageUrl: './_img/dratini.png',
-        type: ['Dragon'],
-        number: '147'
-    },
-    {
-        name: 'nombre X',
-        imageUrl: './_img/dratini.png',
-        type: ['Dragon'],
-        number: '147'
-    },
-    {
-        name: 'nombre X',
-        imageUrl: './_img/dratini.png',
-        type: ['Dragon'],
-        number: '147'
-    },
-    {
-        name: 'nombre X',
-        imageUrl: './_img/dratini.png',
-        type: ['Dragon'],
-        number: '147'
-    }
-]
+let data= [];
 
 //Private Functions Region
+function fetchPokemon(id) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then((res) => res.json())
+    .then((data) => {
+        data.push();
+    });
+} 
+
 function createImageItem(url, name) {
     let img = document.createElement('img');
     img.src = url;
     img.alt = name;
     img.classList.add('card-img-top', 'cardPictures');
-
 
     return img
 }
@@ -112,10 +27,18 @@ function createCardTitle(name) {
     return cardTitle
 }
 
-function createCardText(types, number) {
+function createCardText(key, value) {
     let cardText = document.createElement('p');
     cardText.classList.add('card-text');
-    cardText.textContent = 'Number: ' + number;
+    if(value.length){
+        let content = key + ': '
+        for(let i=0; i<value.length; i++) {
+            content = content + ' ' + value[i].type.name;
+        }
+        cardText.textContent = content;
+    } else {
+        cardText.textContent = key + ': ' + value;
+    }
 
     return cardText
 }
@@ -124,7 +47,8 @@ function createCardBody(_data) {
     let cardBody = document.createElement('div');
     cardBody.classList.add('card-body');
     cardBody.appendChild(createCardTitle(_data.name));
-    cardBody.appendChild(createCardText(_data.types, _data.id))
+    cardBody.appendChild(createCardText('Type', _data.types));
+    cardBody.appendChild(createCardText('Number', _data.id));
 
     return cardBody
 }
@@ -133,15 +57,15 @@ function createCards() {
     let container = document.querySelector('#cardsContainer');
     for (let i=1; i<=PokemonTotalNumber; i++) {
         fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`)
-      .then((res) => res.json())
-      .then((pokemonFetched) => {
-        let pokemon = document.createElement('card');
-        pokemon.classList.add('card', 'cards');
-        pokemon.id = i;
-        pokemon.appendChild(createImageItem(pokemonFetched.sprites.front_default, pokemonFetched.name));
-        pokemon.appendChild(createCardBody(pokemonFetched));
-        container.appendChild(pokemon);
-      });
+        .then((res) => res.json())
+        .then((pokemonFetched) => {
+            let pokemon = document.createElement('card');
+            pokemon.classList.add('card', 'cards');
+            pokemon.id = i;
+            pokemon.appendChild(createImageItem(pokemonFetched.sprites.front_default, pokemonFetched.name));
+            pokemon.appendChild(createCardBody(pokemonFetched));
+            container.appendChild(pokemon);
+        });
     }
 }
 //End Private Functions Region
@@ -152,11 +76,3 @@ document.addEventListener('DOMContentLoaded', function(evt) {
     createCards();
 })
 //End events region
-
-function fetchPokemon(id) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-      .then((res) => res.json())
-      .then((data) => {
-        
-      });
-  }
