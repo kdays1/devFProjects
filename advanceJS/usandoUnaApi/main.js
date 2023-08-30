c22d34fc390ffd7d50d13196fe6c8fa87bf4682d
Kdays <caroodzg@gmail.com>
2023-08-29 18:54:35 -0600

    Pokedex in progress

--- advanceJS/usandoUnaApi/main.js
+++ advanceJS/usandoUnaApi/main.js
@@ -1,5 +1,9 @@
-const characterContainer = document.getElementById('characters')
-const urlAPI = 'https://rickandmortyapi.com/api/character'
+// const characterContainer = document.getElementById('characters')
+// const urlAPI = 'https://rickandmortyapi.com/api/character'
+const PokemonTotalNumber = 1008;
+let pokemonArray = [];
+let orderedPokemonArray = [];
+const promises = [];
 
 function pokemonProtoype(name, imageUrl, types, number) {
     this.name = name;
@@ -8,102 +12,90 @@ function pokemonProtoype(name, imageUrl, types, number) {
     this.number = number;
 }
 
-function displayChar(characters) {
-    characters.forEach(character => {
-        const characterDiv = document.createElement('div')
-
-        characterDiv.innerHTML = `
-        <h2>${character.name}</h2>
-        <img src="${character.image}" alt="${character.name}">
-        `;
-
-        characterContainer.appendChild(characterDiv);
-    })
+function orderPokemonDataStructure(allPokemonInfo) {
+    for(let i=0;i<allPokemonInfo.length;i++) {
+        let name= allPokemonInfo[i].name;
+        let imageUrl= allPokemonInfo[i].sprites.front_default;
+        let types= allPokemonInfo[i].types;
+        let number= allPokemonInfo[i].id;
+        pokemonArray.push(new pokemonProtoype(name, imageUrl, types, number));
+    }
+    createCards(pokemonArray);
+    orderPokemonByName(pokemonArray);
 }
 
+function createImageItem(url, name) {
+    let img = document.createElement('img');
+    img.src = url;
+    img.alt = name;
+    img.classList.add('card-img-top', 'cardPictures');
 
-// function createImageItem(url, name) {
-//     let img = document.createElement('img');
-//     img.src = url;
-//     img.alt = name;
-//     img.classList.add('card-img-top', 'cardPictures');
+    return img
+}
 
-//     return img
-// }
+function createCardTitle(name) {
+    let cardTitle = document.createElement('h5');
+    cardTitle.classList.add('card-title');
+    cardTitle.textContent = 'Name: ' + name;
 
-// function createCardTitle(name) {
-//     let cardTitle = document.createElement('h5');
-//     cardTitle.classList.add('card-title');
-//     cardTitle.textContent = 'Name: ' + name;
+    return cardTitle
+}
 
-//     return cardTitle
-// }
+function createCardText(key, value) {
+    let cardText = document.createElement('p');
+    cardText.classList.add('card-text');
+    if(value.length){
+        let content = key + ': '
+        for(let i=0; i<value.length; i++) {
+            content = i===0 ? content + ' ' + value[i].type.name : content + ' / ' + value[i].type.name;
+        }
+        cardText.textContent = content;
+    } else {
+        cardText.textContent = key + ': ' + value;
+    }
 
-// function createCardText(key, value) {
-//     let cardText = document.createElement('p');
-//     cardText.classList.add('card-text');
-//     if(value.length){
-//         let content = key + ': '
-//         for(let i=0; i<value.length; i++) {
-//             content = i===0 ? content + ' ' + value[i].type.name : content + ' / ' + value[i].type.name;
-//         }
-//         cardText.textContent = content;
-//     } else {
-//         cardText.textContent = key + ': ' + value;
-//     }
+    return cardText
+}
 
-//     return cardText
-// }
+function createCardBody(_data) {
+    let cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    cardBody.appendChild(createCardTitle(_data.name));
+    cardBody.appendChild(document.createElement('hr'));
+    cardBody.appendChild(createCardText('Type', _data.types));
+    cardBody.appendChild(document.createElement('hr'));
+    cardBody.appendChild(createCardText('Number', _data.number));
 
-// function createCardBody(_data) {
-//     let cardBody = document.createElement('div');
-//     cardBody.classList.add('card-body');
-//     cardBody.appendChild(createCardTitle(_data.name));
-//     cardBody.appendChild(document.createElement('hr'));
-//     cardBody.appendChild(createCardText('Type', _data.types));
-//     cardBody.appendChild(document.createElement('hr'));
-//     cardBody.appendChild(createCardText('Number', _data.number));
+    return cardBody
+}
 
-//     return cardBody
-// }
+function createCards(_pokemonArray) {
+    let container = document.querySelector('#cardsContainer');
+    container.innerHTML = '';
+    for (let i=0; i<_pokemonArray.length; i++) {
+        let pokemon = document.createElement('card');
+        pokemon.classList.add('card', 'cards');
+        pokemon.id = _pokemonArray[i].number;
+        pokemon.appendChild(createImageItem(_pokemonArray[i].imageUrl, _pokemonArray.name));
+        pokemon.appendChild(createCardBody(_pokemonArray[i]));
+        container.appendChild(pokemon);
+    }
+}
 
+function createPokemonPromises() {
+    for (let i=1; i<=PokemonTotalNumber; i++) {
+        promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`));
+    }
+    Promise.all(promises)
+        .then((res) => {
+            Promise.all(res.map((item) => {
+                return item.json();
+            }))
+            .then(data => orderPokemonDataStructure(data));
+        })
+        .catch((e) => {
+            console.log('There was an error in the multiple fetch');
+        })
+    }
 
-// function createCards(_pokemonArray) {
-//     let container = document.querySelector('#cardsContainer');
-//     container.innerHTML = '';
-//     for (let i=0; i<_pokemonArray.length; i++) {
-//         let pokemon = document.createElement('card');
-//         pokemon.classList.add('card', 'cards');
-//         pokemon.id = _pokemonArray[i].number;
-//         pokemon.appendChild(createImageItem(_pokemonArray[i].imageUrl, _pokemonArray.name));
-//         pokemon.appendChild(createCardBody(_pokemonArray[i]));
-//         container.appendChild(pokemon);
-//     }
-// }
-
-fetch(urlAPI)
-    .then(response => response.json())
-    .then(data => {
-        const characters = data.results
-        console.log(characters)
-        displayChar(characters)
-    })
-    .catch(error => console.log('Error ', error))
-
-// function createPokemonPromises() {
-//     for (let i=1; i<=PokemonTotalNumber; i++) {
-//         promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`));
-//     }
-//     Promise.all(promises)
-//         .then((res) => {
-//             Promise.all(res.map((item) => {
-//                 return item.json();
-//             }))
-//             .then(data => orderPokemonData(data));
-//         })
-//         .catch((e) => {
-//             console.log('There was an error in the multiple fetch');
-//         })
-//     }
-
-// createPokemonPromises();
+createPokemonPromises();
